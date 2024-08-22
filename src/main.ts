cb91c142634f08bb45b3e067b1e92153832c5f8e
Gabriel-SH3 <gabriel.meireles@sh3.com.br>
2024-08-22 12:01:53 -0300

    [Feature] Estrutura inicial do projeto

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173', // Substitua pelo domínio do aplicativo
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Permitir credenciais (por exemplo, cookies)
+  });
+  
+  await app.listen(3000);
+}
+bootstrap();
